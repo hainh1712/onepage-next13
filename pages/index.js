5753d91d8b8d1de3f88ff0fa73978f0fc0461a5c
hainh1712 <nguyenhoanghai20021712@gmail.com>
2024-03-15 17:14:26 +0700

    update

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,25 @@
 import { useEffect, useState } from "react";
+import Image from "next/image";
 export default function Home() {
   return (
     <div className="bg-[#f2cedf] w-full">
       <h1 className="font-bold text-center">Hello Next.js 13!</h1>
       <div className="images my-2 flex items-center justify-center flex-col">
-        <img
+        <Image
           src="https://cdn.tgdd.vn/Files/2023/02/21/1511939/hoa-anh-dao-nhat-ban-y-nghia-hinh-anh-cach-trong-cham-soc-tai-nha-202302220816312265.jpg"
           alt="Hình ảnh 1"
           width="300"
           height="200"
           className="my-2"
         />
-        <img
+        <Image
           src="https://danviet.mediacdn.vn/upload/2-2018/images/2018-05-08/Loat-anh-hoa-anh-dao-dep-nhu-tien-canh-khien-du-khach-muon-den-Nhat-Ban-ngay-lap-tuc-1-1525769308-width650height366.jpg"
           alt="Hình ảnh 2"
           width="300"
           height="200"
           className="my-2"
         />
-        <img
+        <Image
           src="https://image.plo.vn/w2000/Uploaded/2024/bpcbzqvp/2023_04_03/hoa-anh-dao-1-2477.jpg.webp"
           alt="Hình ảnh 3"
           width="300"
@@ -26,7 +27,7 @@ export default function Home() {
           className="my-2"
         />
         {[...Array(34)].map((_, index) => (
-        <img
+        <Image
           key={index}
           src={`https://itss-hedsocial.s3.amazonaws.com/conan/chap_1/${index + 1}.jpg`}
           alt={`Hình ảnh ${index + 1}`}
